892666b9bb2f8fa2d8bffac9a027d7c21409c383
yyljj <yyljj@outlook.com>
2016-04-28 23:00:29 +0800

    test class point

--- caculate/main.cpp
+++ caculate/main.cpp
@@ -4,15 +4,16 @@ int main()
 {
 	for(;;)
 	{
-		Ccaculate a;
-		a.getOperate();
-		if(a.ifRightOperate()==false)
+		Ccaculate* a = new Ccaculate;
+
+		a->getOperate();
+		if(a->ifRightOperate()==false)
 		{
 			cout<<"请输入正确的操作符"<<endl;
 			continue;
 		}
-		a.getX();
-		a.caculate();
+		a->getX();
+		a->caculate();
 	}
 	
 	return 0;
